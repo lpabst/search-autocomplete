3273885dd2bcf3a0308640d538b1cda91e6891ed
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2017-12-18 14:35:00 -0700

    deleted unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import {unregister} from './registerServiceWorker';
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import store from './store';
-
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={ store }>
+  <HashRouter>
       <App />
-    </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
--- src/store.js
+++
@@ -1,6 +0,0 @@
-import {createStore, applyMiddleware, combineReducers} from 'redux';
-import promiseMiddleware from 'redux-promise-middleware';
-
-import reducer from './ducks/reducer';
-
-export default createStore(reducer, undefined, applyMiddleware( promiseMiddleware() ));
