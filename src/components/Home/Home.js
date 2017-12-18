6c416acb758c1a65233fe1a50583168fb4dd7a1e
Loren Pabst <loren.pabst@ALLEGIANCE.LOCAL>
2017-12-18 16:34:16 -0700

    added functions to select autocomplete options with keys and mouse and to highlight appropriate row

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,10 +9,11 @@ class Home extends Component {
   constructor(props){
     super(props);
     this.state = {
+      words: [],
       userInput: '',
       showAutocompleteBox: true,
       autocompleteSuggestions: [],
-      words: []
+      autocompleteRowHighlighted: -1,
     }
   }
 
@@ -43,10 +44,42 @@ class Home extends Component {
     })
   }
 
+  handleKeyPress(e){
+    if (e.key == 'ArrowDown' || e.key == 'ArrowUp' || e.key == 'Enter'){
+      e.preventDefault();
+    }else{
+      return;
+    }
+
+    let rowHighlighted = this.state.autocompleteRowHighlighted;
+
+    if (e.key == 'ArrowDown' && rowHighlighted < this.state.autocompleteSuggestions.length - 1){
+      rowHighlighted ++;
+    }
+    if (e.key == 'ArrowUp' && rowHighlighted > -1){
+      rowHighlighted --;
+    }
+    if (e.key == 'Enter' && rowHighlighted >= 0){
+      let selection = this.state.autocompleteSuggestions[rowHighlighted];
+      return this.selectAutocomplete(selection);
+    }
+
+    this.setState({
+      autocompleteRowHighlighted: rowHighlighted
+    })
+  }
+
+  setRowHighlighted(index){
+    this.setState({
+      autocompleteRowHighlighted: index
+    })
+  }
+
   selectAutocomplete(item){
     this.setState({
       userInput: item,
-      autocompleteSuggestions: []
+      autocompleteSuggestions: [],
+      autocompleteRowHighlighted: -1
     })
   }
 
@@ -58,7 +91,7 @@ class Home extends Component {
             <img src='https://cdn.vox-cdn.com/uploads/chorus_asset/file/6466217/fixed-google-logo-font.png' alt='google logo' className='google_logo' />
 
             <div className='search_bar'>
-              <input value={this.state.userInput} onChange={ (e) => this.handleUserInput(e) } />
+              <input value={this.state.userInput} onChange={ (e) => this.handleUserInput(e) } onKeyDown={ (e) => this.handleKeyPress(e) } />
               <img src='http://www.androidpolice.com/wp-content/uploads/2015/09/nexus2cee_GoogleLogo2.jpg' alt='voice logo' />
 
               {
@@ -66,7 +99,8 @@ class Home extends Component {
                   <div className='autocomplete_suggestions'>
                     {
                       this.state.autocompleteSuggestions.map( (item, i) => {
-                        return  <div key={i} onClick={() => this.selectAutocomplete(item)} className='autocomplete_suggestions_item'>
+                        let background = (i === this.state.autocompleteRowHighlighted) ? '#ccc' : '#fff';
+                        return  <div key={i} onClick={() => this.selectAutocomplete(item)} onMouseOver={ () => this.setRowHighlighted(i) } style={{background: background}} className='autocomplete_suggestions_item'>
                                   <p>{item}</p>
                                 </div>
                       })
