225458dd6c08a5332b8a890e36d1ec180aa9e628
loren pabst <lorenpabst@gmail.com>
2017-12-19 23:20:58 -0700

    finished adding basics to search.js

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -6,18 +6,14 @@ import data from './../../data.js';
 
 class Search extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       userInput: ''
     }
   }
 
-  componentDidMount(){
-    // console.log('search page mounted')
-  }
-
-  handleUserInput(e){
+  handleUserInput(e) {
     let input = e.target.value;
 
     this.setState({
@@ -26,19 +22,18 @@ class Search extends Component {
   }
 
   render() {
-    console.log('rendering')
     return (
       <div className="search">
-          <div className='search_box'>
+        <div className='search_box'>
 
-            <img src='https://cdn.vox-cdn.com/uploads/chorus_asset/file/6466217/fixed-google-logo-font.png' alt='google logo' className='google_logo' />
+          <img src='https://cdn.vox-cdn.com/uploads/chorus_asset/file/6466217/fixed-google-logo-font.png' alt='google logo' className='google_logo' />
 
-            <div className='search_bar'>
-              <input value={this.state.userInput} onChange={ (e) => this.handleUserInput(e) } />
-              <img src='http://www.androidpolice.com/wp-content/uploads/2015/09/nexus2cee_GoogleLogo2.jpg' alt='voice logo' />
-            </div>
-
+          <div className='search_bar'>
+            <input value={this.state.userInput} onChange={(e) => this.handleUserInput(e)} />
+            <img src='http://www.androidpolice.com/wp-content/uploads/2015/09/nexus2cee_GoogleLogo2.jpg' alt='voice logo' />
           </div>
+
+        </div>
       </div>
     );
   }
